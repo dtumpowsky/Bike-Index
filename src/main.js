47aa1fa9273a9462afa6eb964d4f8c2f818ccfe8
David Tumpowsky and Jimmy McNamara <jimmy@>
2018-05-31 11:40:52 -0700

    Enter a manufacturer and get all stolen bikes from user location with that manufacturer

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('#manufacturer').val("");
 
     let request = new XMLHttpRequest();
-    let url = `https://bikeindex.org:443/api/v3/manufacturers/${brand}`;
+    let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=${brand}&location=IP&distance=10&stolenness=stolen`;
 
 
     request.onreadystatechange = function() {
@@ -18,7 +18,12 @@ $(document).ready(function() {
     request.send();
 
     const getElements = function(response) {
-      $('.showBike').text(`These are the lost bikes with this ${brand} are ${response.manufacturer.name} and an id of ${response.manufacturer.id}`);
+      $('.showBike').text(`These are the lost bikes with the brand ${brand}:`);
+      response.bikes.forEach(function(bikeInfo) {
+        let dateStolen = new Date(`${bikeInfo.date_stolen}`*1000);
+
+        $('.showBike').append(`<ul><li>${bikeInfo.id}</li> <ul><li>${bikeInfo.title}</li> <li>${bikeInfo.serial}</li> <li>${bikeInfo.manufacturer_name}</li> <li>${bikeInfo.frame_model}</li> <li>${bikeInfo.year}</li> <li>${bikeInfo.frame_colors}</li> <li>${bikeInfo.stolen_location}</li> <li>${dateStolen}</li></ul></ul>`);
+      })
     }
   });
 });
